cfe4c53e26f015053e6bfdfc02824aa72670cb9f
LottieDeane <71388149+LottieDeane@users.noreply.github.com>
2021-02-06 11:33:14 +0000

    copy and pasted in
    bahbahbahb

--- PM test.py
+++ PM test.py
@@ -1,27 +1,16 @@
 #!/usr/bin/env python3
 
-import time
 from pms5003 import PMS5003, ReadTimeoutError
-import logging
-
-logging.basicConfig(
-    format='%(asctime)s.%(msecs)03d %(levelname)-8s %(message)s',
-    level=logging.INFO,
-    datefmt='%Y-%m-%d %H:%M:%S')
-
-logging.info("""particulates.py - Print readings from the PMS5003 particulate sensor.
-Press Ctrl+C to exit!
-""")
 
 pms5003 = PMS5003()
-time.sleep(1.0)
 
-try:
-    while True:
-        try:
-            readings = pms5003.read()
-            logging.info(readings)
-        except ReadTimeoutError:
-            pms5003 = PMS5003()
-except KeyboardInterrupt:
-    pass
+while True:
+    try:
+        pm_values = pms5003.read()
+        values["P2"] = str(pm_values.pm_ug_per_m3(2.5))
+        values["P1"] = str(pm_values.pm_ug_per_m3(10))
+    except ReadTimeoutError:
+        pms5003.reset()
+        pm_values = pms5003.read()
+        values["P2"] = str(pm_values.pm_ug_per_m3(2.5))
+        values["P1"] = str(pm_values.pm_ug_per_m3(10))
