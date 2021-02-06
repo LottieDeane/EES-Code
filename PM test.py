c1d4c78c2b858b287a2863075d8805a77a1350d4
LottieDeane <71388149+LottieDeane@users.noreply.github.com>
2021-02-06 11:34:17 +0000

    Update PM test.py

--- PM test.py
+++ PM test.py
@@ -5,12 +5,17 @@ from pms5003 import PMS5003, ReadTimeoutError
 pms5003 = PMS5003()
 
 while True:
+    data = []
     try:
         pm_values = pms5003.read()
-        values["P2"] = str(pm_values.pm_ug_per_m3(2.5))
-        values["P1"] = str(pm_values.pm_ug_per_m3(10))
+        Data.append("PM1.0 ug/m3: "+str(pm_values.pm_ug_per_m3(1)))
+        Data.append("PM2.5 ug/m3: "+str(pm_values.pm_ug_per_m3(2.5)))
+        Data.append("PM10 ug/m3: "+str(pm_values.pm_ug_per_m3(10)))
     except ReadTimeoutError:
         pms5003.reset()
-        pm_values = pms5003.read()
-        values["P2"] = str(pm_values.pm_ug_per_m3(2.5))
-        values["P1"] = str(pm_values.pm_ug_per_m3(10))
+        pm_values = pms5003.read() 
+        Data.append("PM1.0 ug/m3: "+str(pm_values.pm_ug_per_m3(1)))
+        Data.append("PM2.5 ug/m3: "+str(pm_values.pm_ug_per_m3(2.5)))
+        Data.append("PM10 ug/m3: "+str(pm_values.pm_ug_per_m3(10)))
+    for i in data:
+        print(i)
