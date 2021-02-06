ba2efcca2a77a344ba966f73f41cf66540938e53
LottieDeane <71388149+LottieDeane@users.noreply.github.com>
2021-02-06 11:14:14 +0000

    Added PM Sensor Readings
    pm sensor worked first time!!!!!

--- LCD screen.py
+++ LCD screen.py
@@ -1,3 +1,4 @@
+#screen
 from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
@@ -5,13 +6,17 @@ import time
 
 import ST7789
 
-print("""
-shapes.py - Display test shapes on the LCD using PIL.
+#ADC
+import time
+import board
+import busio
+import adafruit_ads1x15.ads1115 as ADS
+from adafruit_ads1x15.analog_in import AnalogIn
 
-If you're using Breakout Garden, plug the 0.96" LCD (SPI)
-breakout into the rear slot.
+#pm sensor
+from pms5003 import PMS5003, ReadTimeoutError
 
-""")
+MESSAGE = "Hello World! How are you today?"
 
 # Create ST7735 LCD display class.
 disp = ST7789.ST7789(
@@ -20,72 +25,62 @@ disp = ST7789.ST7789(
     dc=9,
     backlight=19,               # 18 for back BG slot, 19 for front BG slot.
     rotation=90,
-    spi_speed_hz=4000000
+    spi_speed_hz=10000000
 )
+
+pms5003 = PMS5003()
 
 # Initialize display.
 disp.begin()
 
 WIDTH = disp.width
 HEIGHT = disp.height
-print(disp.width,disp.height)
 
 
-# Clear the display to a red background.
-# Can pass any tuple of red, green, blue values (from 0 to 255 each).
-# Get a PIL Draw object to start drawing on the display buffer.
-img = Image.new('RGB', (WIDTH, HEIGHT), color=(255, 0, 0))
+img = Image.new('RGB', (WIDTH, HEIGHT), color=(0, 0, 0))
 
 draw = ImageDraw.Draw(img)
 
-# Draw a purple rectangle with yellow outline.
-draw.rectangle((10, 10, WIDTH - 10, HEIGHT - 10), outline=(255, 255, 0), fill=(255, 0, 255))
+font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf", 20)
 
-# Draw some shapes.
-# Draw a blue ellipse with a green outline.
-draw.ellipse((10, 10, WIDTH - 10, HEIGHT - 10), outline=(0, 255, 0), fill=(0, 0, 255))
+size_x, size_y = draw.textsize(MESSAGE, font)
 
-# Draw a white X.
-draw.line((10, 10, WIDTH - 10, HEIGHT - 10), fill=(255, 255, 255))
-draw.line((10, HEIGHT - 10, WIDTH - 10, 10), fill=(255, 255, 255))
+text_x = 0
+text_y = 0
 
-# Draw a cyan triangle with a black outline.
-draw.polygon([(WIDTH / 2, 10), (WIDTH - 10, HEIGHT - 10), (10, HEIGHT - 10)], outline=(0, 0, 0), fill=(0, 255, 255))
+t_start = time.time()
 
-# Load default font.
-font = ImageFont.load_default()
+# Create the I2C bus
+i2c = busio.I2C(board.SCL, board.SDA)
 
-# Alternatively load a TTF font.
-# Some other nice fonts to try: http://www.dafont.com/bitmap.php
-# font = ImageFont.truetype('Minecraftia.ttf', 16)
+# Create the ADC object using the I2C bus
+ads = ADS.ADS1115(i2c)
 
+#created singe ended inputs on channels 0, 1, 2 for each sensor
+sensor0 = AnalogIn(ads, ADS.P0)
+sensor1 = AnalogIn(ads, ADS.P1)
+sensor2 = AnalogIn(ads, ADS.P2)
 
-# Define a function to create rotated text.  Unfortunately PIL doesn't have good
-# native support for rotated fonts, but this function can be used to make a
-# text image and rotate it so it's easy to paste in the buffer.
-def draw_rotated_text(image, text, position, angle, font, fill=(255, 255, 255)):
-    # Get rendered font width and height.
-    draw = ImageDraw.Draw(image)
-    width, height = draw.textsize(text, font=font)
-    # Create a new image with transparent background to store the text.
-    textimage = Image.new('RGBA', (width, height), (0, 0, 0, 0))
-    # Render the text.
-    textdraw = ImageDraw.Draw(textimage)
-    textdraw.text((0, 0), text, font=font, fill=fill)
-    # Rotate the text image.
-    rotated = textimage.rotate(angle, expand=1)
-    # Paste the text into the image, using it as a mask for transparency.
-    image.paste(rotated, position, rotated)
+print("{:>5}\t{:>5}".format("raw", "v"))
 
-
-# Write two lines of white text on the buffer, rotated 90 degrees counter clockwise.
-draw_rotated_text(img, 'Hello World!', (0, 0), 90, font, fill=(255, 255, 255))
-draw_rotated_text(img, 'This is a line of text.', (10, HEIGHT - 10), 0, font, fill=(255, 255, 255))
-
-# Write buffer to display hardware, must be called to make things visible on the
-# display!
-disp.display(img)
-print("finished")
-time.sleep(2)
-print("fasd")
+while True:
+    Data = ["Carbon Dioxide","Value: "+str(sensor0.value),"Gas Levels:","Value: "+str(sensor1.value),"Ozone:","Value: "+str(sensor2.value)] 
+    try:
+        pm_values = pms5003.read()
+        Data.append("PM1.0 ug/m3: "+str(pm_values.pm_ug_per_m3(1)))
+        Data.append("PM2.5 ug/m3: "+str(pm_values.pm_ug_per_m3(2.5)))
+        Data.append("PM10 ug/m3: "+str(pm_values.pm_ug_per_m3(10)))
+    except ReadTimeoutError:
+        pms5003.reset()
+        pm_values = pms5003.read() 
+        Data.append("PM1.0 ug/m3: "+str(pm_values.pm_ug_per_m3(1)))
+        Data.append("PM2.5 ug/m3: "+str(pm_values.pm_ug_per_m3(2.5)))
+        Data.append("PM10 ug/m3: "+str(pm_values.pm_ug_per_m3(10)))
+    draw.rectangle((0,0, 200, 240), (0, 0, 0))
+    
+    for i in range(0,len(Data)):
+        #draw.text((text_x, text_y+i*20), Data[i], font=font, fill=(255, 255, 255))
+        print(Data[i])
+    disp.display(img)
+    time.sleep(0.2)
 
