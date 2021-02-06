046f9d12863116b22962e42c53082444d7250c8b
LottieDeane <71388149+LottieDeane@users.noreply.github.com>
2021-02-06 11:57:30 +0000

    Create LCD Test Code.py

---
+++ LCD Test Code.py
@@ -0,0 +1,31 @@
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
+import time
+
+import ST7789
+
+
+MESSAGE = "Hello World! How are you today?"
+
+# Create ST7789 LCD display class.
+disp = ST7789.ST7789(
+    port=0,
+    cs=ST7789.BG_SPI_CS_FRONT,  # BG_SPI_CS_BACK or BG_SPI_CS_FRONT
+    dc=9,
+    backlight=19,               # 18 for back BG slot, 19 for front BG slot.
+    spi_speed_hz=80 * 1000 * 1000
+)
+
+# Initialize display.
+disp.begin()
+
+WIDTH = disp.width
+HEIGHT = disp.height
+
+
+img = Image.new('RGB', (WIDTH, HEIGHT), color=(0, 0, 0))
+draw = ImageDraw.Draw(img)
+font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf", 30)
+
+draw.text((30, 80), MESSAGE, font=font, fill=(255, 255, 255))
